63cd5fe45bdb636029b053e1c8b4f685d7389cb5
soroush <soroush_safarii@yahoo.com>
2020-10-03 12:43:40 +0330

    fix - pep8

--- src/vocab/serializers.py
+++ src/vocab/serializers.py
@@ -3,6 +3,7 @@ from .models import Vocabulary
 
 
 class VocabularySerializer(serializers.ModelSerializer):
+	""" Vocabulary model serialier """
 	class Meta:
 		model = Vocabulary
 		fields = "__all__"
