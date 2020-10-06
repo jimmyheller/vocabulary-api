e45bb50a89abb07f907dddb71ae9cbacc5cbe1bb
soroush <soroush_safarii@yahoo.com>
2020-10-06 17:47:37 +0330

    add - translator unit test

--- src/vocab/tests.py
+++ src/vocab/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 import requests
+from .translator import translator
 
 
 # Create your tests here.
@@ -14,3 +15,12 @@ class RandomAPiTestCases(TestCase):
 		for i in range(50):
 			request = requests.get("http://127.0.0.1:8000/api/v1/random-word/")
 			self.assertEqual(request.status_code, 200)
+
+
+class WordTranslator(TestCase):
+
+	def test_hello(self):
+		self.assertEqual(translator("hello", source='en', destination='fa'), 'سلام')
+
+	def test_admit_wrong(self):
+		self.assertEqual(translator("admit", source='en', destination='fa'), 'اقرار')
