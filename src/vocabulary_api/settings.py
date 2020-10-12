8b2cf0a47e4c3611a8e85ecf8b46cd8fda337da0
soroush <soroush_safarii@yahoo.com>
2020-10-12 09:37:21 +0330

    add - celery

--- src/vocabulary_api/settings.py
+++ src/vocabulary_api/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 	'vocab.apps.VocabConfig',
 	'user.apps.UserConfig',
 	'doc.apps.DocConfig',
+	'scraper.apps.ScraperConfig',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 AUTH_USER_MODEL = 'user.CustomUser'
+
+
+BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'Asia/Tehran'
