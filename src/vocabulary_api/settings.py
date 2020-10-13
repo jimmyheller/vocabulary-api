650885e73fa9ef45e3f62ae405bae6b1ef943950
soroush <soroush_safarii@yahoo.com>
2020-10-13 15:48:55 +0330

    update - settings

--- src/vocabulary_api/settings.py
+++ src/vocabulary_api/settings.py
@@ -123,8 +123,8 @@ STATIC_URL = '/static/'
 AUTH_USER_MODEL = 'user.CustomUser'
 
 
-BROKER_URL = 'redis://localhost:6379'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+BROKER_URL = 'redis://redis:6379'
+CELERY_RESULT_BACKEND = 'redis://redis:6379'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
