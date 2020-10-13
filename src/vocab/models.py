e8b4d23e9cb41331734fb111376a8eac054c841d
soroush <soroush_safarii@yahoo.com>
2020-10-13 15:44:06 +0330

    fix - pep8

--- src/vocab/models.py
+++ src/vocab/models.py
@@ -6,5 +6,6 @@ class Vocabulary(models.Model):
 	word = models.CharField(max_length=50, unique=True)
 	meaning = models.CharField(max_length=50, blank=True, null=True)
 	tag = models.CharField(max_length=50, blank=True, null=True)
+
 	def __str__(self):
 		return self.word
